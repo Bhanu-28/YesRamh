fa3285e33a1dca8677579506b2cd0ab46f855364
bhanu <bhanupradeep123456@gmail.com>
2023-02-19 00:22:42 +0530

    021923//factorialGCDLCMPRACYES

--- src/javabasic/yesRaamh.java
+++ src/javabasic/yesRaamh.java
@@ -5,21 +5,30 @@ import java.util.Scanner;
 public class yesRaamh {
     public static void main(String[] args){
 Scanner sc = new Scanner(System.in);
-int n = sc.nextInt();
-int i =1;
-while(i<=n){
-int j=1;
-while(j<=n){
-    System.out.print(i);
-    j++;
+//int a = sc.nextInt();
+//int b = sc.nextInt();
+//int x = Math.max(a,b);
+//int y = a*b;
+//int ans=x;
+//for(int i=x;i<=y;i++){
+//    if(i%a==0&&i%b==0){
+//        ans=i;
+//        break;
+//    }
+//
+//}
+//        System.out.println(ans);
+
+int a = sc.nextInt();
+int b = sc.nextInt();
+int min = Math.min(a,b);
+int ans = 1;
+for(int i=1;i<=min;i++){
+    if(a%i==0&&b%i==0){
+        ans=i;
+    }
 }
-i++;
-    System.out.println();
-
-}
-
-
-
+        System.out.println(ans);
 
     }
 }
